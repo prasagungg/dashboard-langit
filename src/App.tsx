f6abd7db8f3b1969a4e337d57984d8e5a5900136
prasagungg <agungprasetio878@gmail.com>
2025-07-25 16:49:47 +0700

    status

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { Background, Footer, Navbar } from "./components";
-import { Arrow } from "./icons";
+import { Background, Button, Footer, Navbar } from "./components";
+import { Arrow, GridFour, LineUp, MapIcon, Scan, ViewBoard } from "./icons";
 
 function App() {
   return (
@@ -8,10 +8,14 @@ function App() {
       <div className="py-2 lg:py-6 max-w-4xl mx-10 lg:mx-auto flex flex-col justify-between h-full">
         <Navbar />
         <div className="my-0 lg:my-6 text-center mb-12 lg:mb-0">
-          <div className="grid gap-4 w-full lg:w-5/6 mx-auto">
+          <div className="grid gap-1 lg:gap-4 w-full lg:w-5/6 mx-auto">
+            <h3 className="text-green-400 animate-ping text-base md:text-xl font-bold">
+              Online
+            </h3>
             <h1 className="title-gradient text-xl md:text-4xl font-bold">
-              Langit From Uncle to Everyone
+              Langit From Putra to Everyone
             </h1>
+
             <p className="text-base md:text-xl  text-[#AFAFAF]">
               Experience the internet without boundaries, with Langit. We
               believe in an open and accessible web for everyone, everywhere.
@@ -25,6 +29,9 @@ function App() {
             >
               Login to Dashboard <Arrow />
             </a>
+          </div>
+          <div className="relative md:w-[50%] mx-auto ">
+            <img src="/images/shield.png" className="w-full h-auto" />
           </div>
         </div>
         <Footer />
